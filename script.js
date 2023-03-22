bbf0c6d57fac2e219546713a20ec26e7224301b9
Danielle Gan <DGan@data-mail.com>
2023-03-22 15:54:29 -0400

    random shop name generator

--- script.js
+++ script.js
@@ -2,8 +2,6 @@
 prompt = "What would you like your lettershop to be called?";
 
 shopName = prompt; 
-
-
 // random shop name generator
 
 shop_first = ["funny", "funky", "awkward", "speedy", "direct", "laughable", "perfect", "express"]; 
@@ -11,3 +9,11 @@ shop_last = ["mail", "letter", "package", "communications", "post" ];
 
 
 // on random name generator click, generate two random numbers, concatentate shop_first + " " + shop_last 
+
+rnum1 = (Math.floor(Math.random() * 8));
+rnum2 = (Math.floor(Math.random()* 5));
+
+console.log(rnum1, rnum2);
+
+
+console.log(shop_first[rnum1]+ " " + shop_last[rnum2]);
