9b5019a9cfb0467f938dba58f7dfb80aab66b67f
Danielle Gan <DGan@data-mail.com>
2023-03-22 15:48:22 -0400

    early commit for set up

---
+++ script.js
@@ -0,0 +1,13 @@
+
+prompt = "What would you like your lettershop to be called?";
+
+shopName = prompt; 
+
+
+// random shop name generator
+
+shop_first = ["funny", "funky", "awkward", "speedy", "direct", "laughable", "perfect", "express"]; 
+shop_last = ["mail", "letter", "package", "communications", "post" ]; 
+
+
+// on random name generator click, generate two random numbers, concatentate shop_first + " " + shop_last 
