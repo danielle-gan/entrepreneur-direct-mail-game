c90bf8798a4dbd1e8c71b42e08d481e708a4cad9
Danielle Gan <DGan@data-mail.com>
2023-03-22 16:04:20 -0400

    some variable set up

--- script.js
+++ script.js
@@ -15,5 +15,25 @@ rnum2 = (Math.floor(Math.random()* 5));
 
 console.log(rnum1, rnum2);
 
-
 console.log(shop_first[rnum1]+ " " + shop_last[rnum2]);
+
+// How long would you like to play for? 3 months? 6 months? 12 months? 
+num_months = 3;
+
+// some variable set up 
+money = 10000000; 
+num_clients = 1 
+//  for every $10,000 spent on marketing, increase clients by +1 
+
+// time variables
+current_day = 0; 
+days_left = num_months * 30 - current_day;
+
+// printers
+printers = 1; 
+
+
+// ink = paper: need  at least 
+
+
+
